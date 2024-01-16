d368c8a0336e62d9d2c5dac8eb1d272d22823445
Jihee <sjh709@naver.com>
2024-01-16 20:26:51 +0900

    useQueries 공부

--- src/ReactQueryPage.js
+++ src/ReactQueryPage.js
@@ -1,26 +1,31 @@
+import { useQueries } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
-import { usePostQuery } from './hooks/usePosts';
 
 const ReactQueryPage = () => {
-  const { data, isLoading, isError, error, refetch } = usePostQuery(1);
+  const ids = [1, 2, 3, 4];
 
-  console.log('data, isLoading', data, isLoading);
-  console.log('isError, error', isError, error);
+  const fetchPostDetail = (id) => {
+    return axios.get(`http://localhost:4000/posts/${id}`);
+  };
 
-  if (isLoading) {
-    return <h1>Loading...</h1>;
-  }
-  if (isError) {
-    return <h1>{error.message}</h1>;
-  }
-  return (
-    <div>
-      {/* {data?.map((item, index) => (
-        <div key={index}>{item.title}</div>
-      ))} */}
-      <button onClick={refetch}>post리스트 다시 들고오기</button>
-    </div>
-  );
+  const results = useQueries({
+    queries: ids.map((id) => {
+      return {
+        queryKey: ['posts', id],
+        queryFn: () => fetchPostDetail(id),
+      };
+    }),
+    combine: (results) => {
+      return {
+        data: results.map((result) => result.data),
+      };
+    },
+  });
+
+  console.log('results', results);
+
+  return <div></div>;
 };
 
 export default ReactQueryPage;
